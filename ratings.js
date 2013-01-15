6259e0e41314daa304110fa2b96cc5a55c6bd79c
Andrew Couch <andy@couchand.com>
2013-01-15 09:34:09 -0500

    example: begin to add ratings dimensions

---
+++ ratings.js
@@ -0,0 +1,227 @@
+var ratings = [
+	{
+		i:		1,
+		appearance:	1,
+		aroma:		10,
+		flavor:		8,
+		palate:		1,
+		overall:	2,
+		rating:		"+/-"
+	},
+	{
+		i:		2,
+		appearance:	2,
+		aroma:		3,
+		flavor:		9,
+		palate:		0,
+		overall:	9,
+		rating:		"++"
+	},
+	{
+		i:		3,
+		appearance:	4,
+		aroma:		8,
+		flavor:		4,
+		palate:		2,
+		overall:	19,
+		rating:		"-"
+	},
+	{
+		i:		4,
+		appearance:	0,
+		aroma:		5,
+		flavor:		1,
+		palate:		5,
+		overall:	19,
+		rating:		"+/-"
+	},
+	{
+		i:		5,
+		appearance:	3,
+		aroma:		5,
+		flavor:		5,
+		palate:		5,
+		overall:	15,
+		rating:		"--"
+	},
+	{
+		i:		6,
+		appearance:	5,
+		aroma:		2,
+		flavor:		1,
+		palate:		2,
+		overall:	18,
+		rating:		"--"
+	},
+	{
+		i:		7,
+		appearance:	2,
+		aroma:		5,
+		flavor:		4,
+		palate:		3,
+		overall:	10,
+		rating:		"+"
+	},
+	{
+		i:		8,
+		appearance:	0,
+		aroma:		9,
+		flavor:		7,
+		palate:		3,
+		overall:	5,
+		rating:		"++"
+	},
+	{
+		i:		9,
+		appearance:	3,
+		aroma:		2,
+		flavor:		2,
+		palate:		5,
+		overall:	6,
+		rating:		"-"
+	},
+	{
+		i:		10,
+		appearance:	2,
+		aroma:		0,
+		flavor:		7,
+		palate:		2,
+		overall:	9,
+		rating:		"++"
+	},
+	{
+		i:		11,
+		appearance:	2,
+		aroma:		1,
+		flavor:		1,
+		palate:		0,
+		overall:	14,
+		rating:		"-"
+	},
+	{
+		i:		12,
+		appearance:	0,
+		aroma:		2,
+		flavor:		5,
+		palate:		0,
+		overall:	18,
+		rating:		"--"
+	},
+	{
+		i:		13,
+		appearance:	1,
+		aroma:		0,
+		flavor:		10,
+		palate:		4,
+		overall:	18,
+		rating:		"-"
+	},
+	{
+		i:		14,
+		appearance:	3,
+		aroma:		0,
+		flavor:		3,
+		palate:		5,
+		overall:	1,
+		rating:		"-"
+	},
+	{
+		i:		15,
+		appearance:	2,
+		aroma:		0,
+		flavor:		0,
+		palate:		4,
+		overall:	10,
+		rating:		"+"
+	},
+	{
+		i:		16,
+		appearance:	2,
+		aroma:		10,
+		flavor:		4,
+		palate:		4,
+		overall:	10,
+		rating:		"+/-"
+	},
+	{
+		i:		17,
+		appearance:	0,
+		aroma:		9,
+		flavor:		1,
+		palate:		2,
+		overall:	16,
+		rating:		"+/-"
+	},
+	{
+		i:		18,
+		appearance:	1,
+		aroma:		3,
+		flavor:		2,
+		palate:		1,
+		overall:	7,
+		rating:		"+/-"
+	},
+	{
+		i:		19,
+		appearance:	3,
+		aroma:		1,
+		flavor:		6,
+		palate:		4,
+		overall:	16,
+		rating:		"-"
+	},
+	{
+		i:		20,
+		appearance:	2,
+		aroma:		2,
+		flavor:		1,
+		palate:		1,
+		overall:	13,
+		rating:		"+/-"
+	},
+	{
+		i:		21,
+		appearance:	4,
+		aroma:		4,
+		flavor:		6,
+		palate:		1,
+		overall:	19,
+		rating:		"+"
+	},
+	{
+		i:		22,
+		appearance:	0,
+		aroma:		8,
+		flavor:		9,
+		palate:		4,
+		overall:	10,
+		rating:		"+/-"
+	},
+	{
+		i:		23,
+		appearance:	4,
+		aroma:		4,
+		flavor:		10,
+		palate:		5,
+		overall:	5,
+		rating:		"++"
+	},
+	{
+		i:		24,
+		appearance:	4,
+		aroma:		3,
+		flavor:		2,
+		palate:		5,
+		overall:	19,
+		rating:		"+"
+	},
+	{
+		i:		25,
+		appearance:	4,
+		aroma:		5,
+		flavor:		4,
+		palate:		0,
+		overall:	0,
+		rating:		"--"
+	}
+]
